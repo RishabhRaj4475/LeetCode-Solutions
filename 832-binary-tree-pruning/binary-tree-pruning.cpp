5d1a81accc54f5bbcfab017cc835446440dffab1
Rishabh Raj <64744273+RishabhRaj4475@users.noreply.github.com>
2024-07-18 01:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (6.07%) - LeetSync

---
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* isOnePresent(TreeNode* root){
+        if( root == NULL) return NULL;
+
+        root->left = isOnePresent(root->left);
+        root->right = isOnePresent(root->right);
+
+        if(root->right == NULL and root->left == NULL and root->val != 1) return NULL;
+
+        return root;
+    }
+    TreeNode* pruneTree(TreeNode* root) {
+
+        if( root == NULL) return nullptr;
+
+        root->left = isOnePresent(root->left);
+        root->right = isOnePresent(root->right);
+
+        if(root->left == NULL and root->right == NULL and root->val == 0) return NULL;
+
+        return root;
+        
+    }
+};
