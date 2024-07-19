2bd201a0e42ce9f3a10b72a0c5141442227b1317
Rishabh Raj <64744273+RishabhRaj4475@users.noreply.github.com>
2024-07-19 18:25:10 +0530

    Time: 10 ms (51.56%) | Memory: 19.7 MB (33.42%) - LeetSync

---
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int N = seats.size();
+        int K = 0;
+        int ans = 0;
+
+        for (int i = 0; i < N; ++i) {
+            if (seats[i] == 1) {
+                K = 0;
+            } else {
+                K++;
+                ans = max(ans, (K + 1) / 2);
+            }
+        }
+
+        for (int i = 0; i < N; ++i)  if (seats[i] == 1) {
+            ans = max(ans, i);
+            break;
+        }
+
+        for (int i = N-1; i >= 0; --i)  if (seats[i] == 1) {
+            ans = max(ans, N - 1 - i);
+            break;
+        }
+
+        return ans;
+    }
+};
